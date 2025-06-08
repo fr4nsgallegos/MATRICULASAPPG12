61100db818c9a2e62a6b1288669b11724c1ded5c
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-06-08 12:48:11 -0500

    recuperando valor del textfield-

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:matriculasappg12/pages/home_page.dart';
+import 'package:matriculasappg12/pages/text_field_example_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+  runApp(
+    MaterialApp(
+      home: TextFieldExamplePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
---
+++ lib/pages/text_field_example_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class TextFieldExamplePage extends StatelessWidget {
+  String? nombre;
+  TextEditingController _nombreController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("TEXT FIELD")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Center(
+          child: Column(
+            children: [
+              TextField(
+                controller: _nombreController,
+                style: TextStyle(
+                  color: Colors.red,
+                ), //controla el color del texto ingresado por el usuario
+                maxLength: 50, //el máximo de caracteres en la respuesta
+                decoration: InputDecoration(
+                  counterText: "", //ocultar contador de caracteres
+                  labelText:
+                      "Nombre", //texto de etiqueta que aparece cuando el field no esta seleccionado
+                  hintText:
+                      "Ingres el nombre completo", //tendo de ayuda dentro del campo
+                  prefixIcon: Icon(Icons.person), //Icono a la izquierda
+                ),
+                // onChanged: (valor) {
+                //   nombre = valor;
+                //   print(nombre);
+                // },
+              ),
+              SizedBox(height: 64),
+              ElevatedButton(
+                onPressed: () {
+                  print("-----------------------");
+                  print(_nombreController.text);
+                  print("-----------------------");
+                },
+                child: Text("Impimir el valor"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
